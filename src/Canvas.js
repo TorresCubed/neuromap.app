73d650f020691e04b9d4ed51cfd3f59db9e49a6f
Alexis Torres <torres.alexis04@yahoo.com>
2021-01-04 23:31:21 -0800

    Setup basic layout and add in "idea" entries

---
+++ src/Canvas.js
@@ -0,0 +1,19 @@
+
+import React, { useState, useCallback}  from 'react';
+import FreeForm from './FreeformIdeas';
+import Options from './OptionBar';
+
+
+const Window = () => {
+    const [hideSourceOnDrag, setHideSrouceOnDrag] = useState(true);
+    const toggle = useCallback(() => setHideSrouceOnDrag(!hideSourceOnDrag), [
+        hideSourceOnDrag,
+    ]);
+    return (<div className="Containment">
+            <FreeForm id="freeFormMap" hideSourceOnDrag={hideSourceOnDrag}/>
+            <Options/>
+    </div>);
+};
+
+
+export default Window;
