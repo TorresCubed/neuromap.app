0ee3159b9c057f32faaaa133957cff763c7d9795
OscarMarshall <OscarMarshall@users.noreply.github.com>
2021-01-07 07:38:09 +0000

    Prettified Code!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import Window from './Canvas';
-import './App.css';
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd';
-
+import React from "react";
+import Window from "./Canvas";
+import "./App.css";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
 
 class App extends React.Component {
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>
-            Map Your Ideas Below
-          </h1>
+          <h1>Map Your Ideas Below</h1>
         </header>
 
         <div>
-          <DndProvider backend= {HTML5Backend}>
+          <DndProvider backend={HTML5Backend}>
             <Window />
           </DndProvider>
         </div>
@@ -25,8 +21,5 @@ class App extends React.Component {
     );
   }
 }
-
-
-
 
 export default App;
--- src/Canvas.js
+++ src/Canvas.js
@@ -1,19 +1,18 @@
-
-import React, { useState, useCallback}  from 'react';
-import FreeForm from './FreeformIdeas';
-import Options from './OptionBar';
-
+import React, { useState, useCallback } from "react";
+import FreeForm from "./FreeformIdeas";
+import Options from "./OptionBar";
 
 const Window = () => {
-    const [hideSourceOnDrag, setHideSrouceOnDrag] = useState(true);
-    const toggle = useCallback(() => setHideSrouceOnDrag(!hideSourceOnDrag), [
-        hideSourceOnDrag,
-    ]);
-    return (<div className="Containment">
-            <FreeForm id="freeFormMap" hideSourceOnDrag={hideSourceOnDrag}/>
-            <Options/>
-    </div>);
+  const [hideSourceOnDrag, setHideSrouceOnDrag] = useState(true);
+  const toggle = useCallback(() => setHideSrouceOnDrag(!hideSourceOnDrag), [
+    hideSourceOnDrag,
+  ]);
+  return (
+    <div className="Containment">
+      <FreeForm id="freeFormMap" hideSourceOnDrag={hideSourceOnDrag} />
+      <Options />
+    </div>
+  );
 };
-
 
 export default Window;
